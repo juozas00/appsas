d6174311b88f421769aa6a1cbdf5c22cda25c37f
juozas00 <nowadayss@yahoo.com>
2023-10-27 23:58:01 +0300

    pak

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("veikia")
+//const numberSpan = document.querySelector(".span")
+
+//numberSpan.style.color = "aqua"
+
+const numberSpan = document.querySelector (".span")
+const buttons = document.querySelector(".buttons_conteiner")
+
+buttons.addEventListener("click",(ev)=> {
+
+    if(ev.target.classList.contains
+("substract")) {
+  numberSpan.innerHTML--
+  setColor()
+}
+
+  if(ev.target.classList.contains
+("add")) {
+  numberSpan.innerHTML++
+  setColor
+}
+   if(ev.target.classList.contains
+("reset")) {
+   numberSpan.innerHTML = 0
+}
+setColor()
+})
+
+const setColor = () => {
+    if (numberSpan.innerHTML>0){
+    numberSpan.style.color = "yellow"
+} 
+else   if (numberSpan.innerHTML<0){
+ numberSpan.style.color = "red"
+}
+else {
+    numberSpan.style.color = "white"
+    }
+};
+
+
+
+//const substract = document.querySelector(".substract")
+//const reset = document.querySelector(".reset")
+//const add = document.querySelector(".add")
+
+
+
+//substract.addEventListener("click", ()=>{
+    numberSpan.innerHTML--
+//});
+//add.addEventListener("click", ()=>{
+    numberSpan.innerHTML++
+//});
+//reset.addEventListener("click", ()=>{
+    numberSpan.innerHTML = 0
+//});    
